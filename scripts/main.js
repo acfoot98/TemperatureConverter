bf97f4246c7c3d362ba1066b9a8b41cc94951f10
Aili Lightfoot <alightfoot@atl.yearup.org>
2022-09-30 20:34:32 -0400

    conversion

---
+++ scripts/main.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", ()=>{
+    //ADD OUR DEFERRED CODE HERE
+
+    //FIND ALL INPUTS
+    const inputList = document.getElementsByTagName("input")
+    //IF THERE ARE ANY
+    if(inputList.length > 0){
+        // SET FOCUS TO THE FIRST ONE
+        inputList[0].focus()
+    }
+
+});
